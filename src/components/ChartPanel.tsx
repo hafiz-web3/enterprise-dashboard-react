7699db5f6c03abe9068dccd64553fd66bde07b47
Hafiz <w.m.hafiz30@gmail.com>
2026-01-29 14:09:11 +0800

    Add chart panel placeholder

---
+++ src/components/ChartPanel.tsx
@@ -0,0 +1,7 @@
+export default function ChartPanel() {
+  return (
+    <div style={{ border: "1px dashed #ccc", padding: 16, borderRadius: 8 }}>
+      KPI trends / usage charts (placeholder)
+    </div>
+  );
+}
